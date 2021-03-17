fd402bfcca7bf01321fcb30e7bad20177f142866
gabriel.brito <gabriefranccelino@gmail.com>
2021-03-17 08:13:29 -0300

    feat: style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { GlobalStyles } from './styles/global'
+
 export const App: React.FC = () => {
   return (
     <div>
       <h1>Hello create-app</h1>
+      <div style={{ fontSize: '1.6rem' }}>Teste 16</div>
+      <div style={{ fontSize: '16px' }}>Teste 16</div>
+      <div style={{ fontSize: '1.4rem' }}>Teste 14</div>
+      <div style={{ fontSize: '1rem' }}>Teste 10</div>
+      <GlobalStyles />
     </div>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --shape: #fff;
+
+    --red: #E52e4D;
+
+    --blue: #5429CC;
+    --blue-light: #6933ff;
+
+    --green: #33CC95;
+
+    --text-title: #363F5F;
+    --text-body: #969cb3;
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+  }
+
+  html {
+
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+`
