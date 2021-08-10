de17f3ddef985af6e467c66bb6a94a39c3f84d70
Gabriel <gabrielfranccelino@gmail.com>
2021-08-10 06:27:24 -0300

    test: button

--- src/components/Button/button.unit.spec.tsx
+++ src/components/Button/button.unit.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/test/helpers'
+import theme from '../../styles/theme'
 
 import { Button } from '.'
 
@@ -8,8 +9,20 @@ describe('<Button />', () => {
   it('should render the button', () => {
     renderWithTheme(<Button>Teste Button</Button>)
 
-    expect(
-      screen.getByRole('button', { name: /teste button/i })
-    ).toBeInTheDocument()
+    const button = screen.getByRole('button', { name: /teste button/i })
+
+    expect(button).toBeInTheDocument()
+  })
+
+  it('should render the button with style', () => {
+    renderWithTheme(<Button>Teste Button</Button>)
+
+    const button = screen.getByRole('button', { name: /teste button/i })
+
+    expect(button).toHaveStyle({
+      backgroundColor: `${theme.colors.blueLight}`,
+      color: `${theme.colors.white}`,
+      border: 0
+    })
   })
 })
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,6 +4,6 @@ import * as S from './styles'
 
 type ButtonTypes = ButtonHTMLAttributes<HTMLButtonElement>
 
-export const Button: React.FC<ButtonTypes> = ({ children }) => {
-  return <S.Button>{children}</S.Button>
+export const Button: React.FC<ButtonTypes> = ({ children, ...props }) => {
+  return <S.Button {...props}>{children}</S.Button>
 }
